30415eebfbd5ca98a51b939d1367087226bf19af
sokey <126024643+SOK3Y@users.noreply.github.com>
2024-04-13 23:09:27 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'sokey.pro (sokey)'
+version '1.0.0'
+
+client_scripts {
+    'main.lua',
+}
+
+shared_scripts {
+    'config.lua',
+    '@es_extended/imports.lua',
+    '@ox_lib/init.lua'
+}
